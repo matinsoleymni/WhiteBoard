346a18bda4e0ecf1ff28515d44075b3f2bd5e163
Matin Soleymani <matinsoleymni@gmail.com>
2023-07-21 17:38:44 +0330

    add undo option and delete all option

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,34 @@ const $ = document;
 const header = $.querySelector('header');
 const color = $.querySelector('#brushColor');
 const width = $.querySelector('#brushWidth');
-const changeToggle = $.querySelector('#changeToggle');
+const undo = $.querySelector('#undoBtn');
+const deleteBtn = $.querySelector('#deleteBtn');
 const whiteBoard = $.querySelector('.whiteBoard');
 
-changeToggle.addEventListener('click' , ()=> {
-    console.log(color.value);
-    header.style.backgroundColor = color.value
+whiteBoard.addEventListener('mousemove' , (e)=> {
+    if(e.altKey){
+
+    }else {
+        let brush = $.createElement('div');
+        brush.setAttribute('class' , 'brush');
+        brush.style.top = `${e.y-75}px`;
+        brush.style.left =`${e.x}px`;
+        brush.style.width = `${width.value}px`;
+        brush.style.height = `${width.value}px`;
+        brush.style.backgroundColor = color.value;
+        whiteBoard.append(brush)
+    }
 })
 
-whiteBoard.addEventListener('mousemove' , (e)=> {
-    console.log(width.value);
-    let brush = $.createElement('div');
-    brush.setAttribute('class' , 'brush');
-    brush.style.top = `${e.y-75}px`;
-    brush.style.left =`${e.x}px`;
-    brush.style.width = `${width.value}px`;
-    brush.style.height = `${width.value}px`;
-    brush.style.backgroundColor = color.value;
-    whiteBoard.append(brush)
+undo.addEventListener('click' , ()=> {
+    if(whiteBoard.childElementCount > 0){
+        whiteBoard.lastChild.remove()
+    }
+})
+
+deleteBtn.addEventListener('click' , ()=> {
+    let all = Array.from(whiteBoard.childNodes)
+    all.forEach((item)=> {
+        item.remove();
+    })
 })

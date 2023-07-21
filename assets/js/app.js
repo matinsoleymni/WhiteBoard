39d8690effe597ab74365802ba82ef4361cfca55
Matin Soleymani <matinsoleymni@gmail.com>
2023-07-21 17:16:58 +0330

    add brush width

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 const $ = document;
 const header = $.querySelector('header');
 const color = $.querySelector('#brushColor');
+const width = $.querySelector('#brushWidth');
 const changeToggle = $.querySelector('#changeToggle');
 const whiteBoard = $.querySelector('.whiteBoard');
 
@@ -10,10 +11,13 @@ changeToggle.addEventListener('click' , ()=> {
 })
 
 whiteBoard.addEventListener('mousemove' , (e)=> {
+    console.log(width.value);
     let brush = $.createElement('div');
     brush.setAttribute('class' , 'brush');
     brush.style.top = `${e.y-75}px`;
     brush.style.left =`${e.x}px`;
+    brush.style.width = `${width.value}px`;
+    brush.style.height = `${width.value}px`;
     brush.style.backgroundColor = color.value;
     whiteBoard.append(brush)
 })
